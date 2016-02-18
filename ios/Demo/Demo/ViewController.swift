634423e19ae6aba5120bc913255a1e226d009720
Pedro Jorquera <pjorquera@okode.com>
2016-02-18 22:57:06 +0100

    Refactor

--- ios/Demo/Demo/ViewController.swift
+++ ios/Demo/Demo/ViewController.swift
@@ -7,20 +7,21 @@
 import UIKit
 import MobileForms
 
-class ViewController: Form, FormDelegate {
+class ViewController: UIViewController, FormDelegate {
+
+    @IBOutlet weak var form: Form!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        delegate = self
-        
+        form.delegate = self
         let filepath = NSBundle.mainBundle().pathForResource("form", ofType: "json", inDirectory: "json")
         do {
-            let contents = try NSString(contentsOfFile: filepath!, usedEncoding: nil) as String
-            setForm(contents)
-            loadForm()
+            form.layout =  try NSString(contentsOfFile: filepath!, usedEncoding: nil) as String
+            form.load()
         } catch { }
+        
     }
-
+    
     func result(data: String) {
         print("Result: \(data)")
     }
@@ -28,7 +29,6 @@ class ViewController: Form, FormDelegate {
     func event(eventType: FormEventType, element: String, value: String) {
         print("Event: \(eventType), Element: \(element), Value: \(value)")
     }
-
     
 }
 
